ac0287e80d89a7b71d116c7c4f948339c35aaa67
Chris Francis <chris.francis@bitesite.ca>
2021-04-10 13:10:47 -0400

    Added README and removed setup of gemset

---
+++ README.md
@@ -0,0 +1,63 @@
+# Rails Template
+
+This repository contains a rails application [template](https://guides.rubyonrails.org/rails_application_templates.html). This will create and setup a general rails application with React. [Rvm](https://rvm.io/) must be installed to manage the application's gems. The template assumes the `.railsrc` file is present and the flags are used during creation of a new rails application.
+
+## Pre Steps
+
+- Install and configure [rvm](https://rvm.io/) with your desired version of ruby.
+- Change terminal directory to the location you will be storing your application.
+- Create a gemset on your desired ruby version. Spell it the same as you will be spelling the name of your rails application.
+  - `$ rvm use <RUBY-VERSION>@<APPNAME> --create`
+- Install the rails gem. This is needed when you run `$ rails new ...`
+  - `$ gem install rails`
+
+## Usage Steps
+
+1.  Ensure the Pre Steps have been completed and your environment is setup.
+2.  Run `rails new` with the template. Use the command that fits your needs
+
+- Using a `.railsrc` file:
+  - `$ rails new <APPNAME> -m <THIS TEMPLATE SRC>`
+- Not using a `.railsrc` file:
+  - `$ rails new <APPNAME> --database=postgresql --skip-test --skip-turbolinks --webpack=react --skip-coffee -m <THIS TEMPLATE SRC>`
+
+3.  Follow the cli input instructions
+
+## Template Gem Additions and Features
+
+- Gems:
+
+  - devise
+  - cancancan
+  - rolify
+  - deployem
+  - rabl
+  - gon
+  - resque", require: "resque/server
+  - font-awesome-rails
+  - spire
+  - aws-sdk-s3
+  - pry
+  - rspec-rails
+  - factory_bot_rails
+  - faker
+
+- Sets up:
+
+  - Postgres Database
+  - Removal of unnecessary files
+
+- Optionally sets up:
+
+  - Devise
+  - Cancancan
+  - Rolify
+  - Rspec
+  - Initial git comit and push to repository
+  - ... and more (to be added in future releases)
+
+_Note: If you have any problems running this application using the above steps please submit an issue. If you found your own solution feel free to submit a pull request updating this README. Thanks for your help!_
+
+Contributors:
+
+- [Christopher Francis](https://github.com/7chris71000)
--- template.rb
+++ template.rb
@@ -1,11 +1,5 @@
 =begin
-Template Title: Generic Rails/React User App
-Author: Christopher Francis
-Run Command: $ rails new <APPNAME> -m <THIS TEMPLATE SRC>
-
-This template uses rvm to create and setup a new rails project. It will use the system default ruby version.
-
-Note: use a ~/.railsrc to set your flags. See more here https://samuelmullen.com/articles/configuring_new_rails_projects_with_railsrc_and_templates/#railsrc
+Please follow repository's README.md to use this template
 =end
 
 def source_paths
@@ -15,60 +9,82 @@ end
 def setup_gems
   say "Setting up Gems", :blue
 
-  gem 'devise'
-  gem 'cancancan'
-  gem 'rolify'
-  gem 'deployem'
-  gem 'rabl'
-  gem 'gon'
-  gem 'resque', require: 'resque/server'
-  gem 'font-awesome-rails'
-  gem 'spire', git: 'https://github.com/bitesite/spire-ruby'
-  gem 'aws-sdk-s3', '~> 1'
-  
+  gem "devise"
+  gem "cancancan"
+  gem "rolify"
+  gem "deployem"
+  gem "rabl"
+  gem "gon"
+  gem "resque", require: "resque/server"
+  gem "font-awesome-rails"
+  gem "spire", git: "https://github.com/bitesite/spire-ruby"
+  gem "aws-sdk-s3", "~> 1"
+
   gem_group :development do
-    gem 'pry'
+    gem "pry"
   end
-  
+
   gem_group :development, :test do
-    gem 'rspec-rails', '~> 5.0.0'
-    gem 'factory_bot_rails'
-    gem 'faker'
+    gem "rspec-rails", "~> 5.0.0"
+    gem "factory_bot_rails"
+    gem "faker"
   end
-  
+
   # This used to work then it didnt... ¯\_(ツ)_/¯
-  # insert_into_file 'Gemfile', "#ruby-gemset=#{@app_path}"
+  insert_into_file "Gemfile", "#ruby-gemset=#{@app_path}"
 end
 
 def setup_gemset
-  say "Setting up gemset", :blue
+  # say "Setting up gemset", :blue
+  # run "rm .ruby-version"
 
-  rvm_list = `rvm list`.gsub(Regexp.new("\e\\[.?.?.?m"), "")
-  desired_ruby = rvm_list.match(/=\* (ruby[^ ]+)|=> (ruby[^ ]+)/)[1]
-  require 'pry'; binding.pry
+  # rvm_list = `rvm list`.gsub(Regexp.new("\e\\[.?.?.?m"), "")
+  # desired_ruby = rvm_list.match(/=\* (ruby[^ ]+)|=> (ruby[^ ]+)/)[1]
 
   # Create the gemset
-  run "rvm #{desired_ruby} gemset create #{@app_path}"
-  @rvm = "rvm use #{desired_ruby}@#{@app_path}"
-  run "#{@rvm} gem install bundler"
+  # run "rvm #{desired_ruby} gemset create #{@app_path}"
+  # run "rvm use #{desired_ruby}@#{@app_path}"
+  # require "pry"; binding.pry
+  # # run "gem install bundler"
+
+  # # Need to strip colors in case rvm_pretty_print_flag is enabled in user"s .rvmrc
+  # rvm_list = `rvm list`.gsub(Regexp.new("\e\\[.?.?.?m"), "")
+
+  # desired_ruby = rvm_list.match(/=\* (ruby[^ ]+)|=> (ruby[^ ]+)/)[1]
+  # gemset_name = @app_name
+
+  # # Create the gemset
+  # run "rvm #{desired_ruby} gemset create #{gemset_name}"
+
+  # # Let us run shell commands inside our new gemset. Use this in other template partials.
+  # @rvm = "rvm use #{desired_ruby}@#{gemset_name}"
+
+  # # Create .rvmrc
+  # file ".rvmrc", @rvm
+
+  # # Make the .rvmrc trusted
+  # run "rvm rvmrc trust #{@app_path}"
+
+  # # Since the gemset is likely empty, manually install bundler so it can install the rest
+  run "gem install bundler"
 end
 
 def setup_devise
   say "Setting up Devise", :blue
-  generate 'devise:install'
-  environment 'config.action_mailer.default_url_options = { host: \'localhost\', port: 3000 }', env: 'development'
+  generate "devise:install"
+  environment 'config.action_mailer.default_url_options = { host: \'localhost\', port: 3000 }', env: "development"
   # maybe ask if they want to setup the production.rb
-  generate :devise, 'User'
+  generate :devise, "User"
 end
 
 def setup_cancancan
   say "Setting up CanCanCan", :blue
-  generate 'cancan:ability'
+  generate "cancan:ability"
 end
 
 def setup_rolify
   say "Setting up Rolify", :blue
-  generate :rolify, 'Role', 'User'
+  generate :rolify, "Role", "User"
 end
 
 def setup_rspec
@@ -78,27 +94,26 @@ end
 
 def setup_db
   say "Setting up database", :blue
-  rails_command 'db:create'
-  rails_command 'db:migrate'
+  rails_command "db:create"
+  rails_command "db:migrate"
 end
 
 def setup_react
   say "Adding components folder for React", :blue
   in_root do
-    run 'mkdir app/javascript/components'
+    run "mkdir app/javascript/components"
   end
 end
 
 def setup_controllers
   say "Generating Pages Controller", :blue
-  generate(:controller, 'Pages', 'home')
+  generate(:controller, "Pages", "home")
   route 'root to: \'pages#home\''
 end
 
 def remove_unnecessary_files
-  run 'rm .ruby-version'
-  run 'rm app/javascript/packs/hello_react.jsx'
-  
+  run "rm .ruby-version"
+  run "rm app/javascript/packs/hello_react.jsx"
 end
 
 def get_source_control_repo_name
@@ -112,11 +127,12 @@ source_paths
 setup_gems
 setup_gemset
 
-
 after_bundle do
   setup_react
   setup_controllers
-  
+
+  setup_db
+
   if yes?("\nWill this project require Devise?")
     setup_devise
   end
@@ -130,23 +146,20 @@ after_bundle do
   end
 
   if yes?("Would you like to test this project using RSpec?")
-    setup_devise
     setup_rspec
   end
-  
-  setup_db
+
 
   remove_unnecessary_files
-  
+
   # git :init
   # git add: '.'
   # git commit: '-a -m \'Initial commit\''
-  
+
   # if yes?("Would you like to push this initial commit to your GitHub/BitBucket/GitLab/etc?")
   #   git remote: "add origin #{get_source_control_repo_name}"
   # end
-  
+
   run "cd #{@app_name}"
   say "#{@app_name} successfully created! 😀", :blue
 end
-
